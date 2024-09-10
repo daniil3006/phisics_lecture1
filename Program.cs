40130b69b8ef674760a6c670d9eac71810b6bb60
Daniil Stankevich <144243377+daniil3006@users.noreply.github.com>
2024-09-10 20:08:19 +0300

    update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,6 +1,4 @@
-﻿using System;
-
-namespace project;
+﻿namespace project;
 
 
 class Program
@@ -8,8 +6,8 @@ class Program
     static void CartesianToPolar(double x, double y, int precision)
     {
         double r = Math.Sqrt(x * x + y * y);
-        double phi = Math.Atan2(y, x);
-        Console.WriteLine($"ρ = {Math.Round(r, precision)}, φ = {Math.Round(phi, precision)} rad");
+        double phi = Math.Atan(y / x);
+        Console.WriteLine($"r = {Math.Round(r, precision)}, phi = {Math.Round(phi, precision)} rad");
     }
 
     static void PolarToCartesian(double r, double phi, int precision)
@@ -24,8 +22,8 @@ class Program
         double r = Math.Sqrt(x * x + y * y + z * z);
         double theta = Math.Acos(z / Math.Sqrt(x * x + y * y + z * z));
         double phi = Math.Atan(y / x);
-        Console.WriteLine($"ρ = {Math.Round(r, precision)}, θ = {Math.Round(theta, precision)}, " +
-                          $"φ = {Math.Round(phi, precision)}");
+        Console.WriteLine($"r = {Math.Round(r, precision)}, theta = {Math.Round(theta, precision)}, " +
+                          $"phi = {Math.Round(phi, precision)}");
     }
 
     static void SphericalToCartesian(double r, double theta, double phi, int precision)
@@ -41,7 +39,7 @@ class Program
     {
         double r = Math.Sqrt(x * x + y * y);
         double phi = Math.Atan(y / x);
-        Console.WriteLine($"ρ = {Math.Round(r, precision)}, φ = {Math.Round(phi, precision)}, " +
+        Console.WriteLine($"r = {Math.Round(r, precision)}, phi = {Math.Round(phi, precision)}, " +
                          $"z = {Math.Round(z, precision)}");
     }
 
@@ -55,83 +53,94 @@ class Program
 
     static void Main()
     {
-        Console.WriteLine("Выберите опцию: \n1 - из декартовых координат в полярные \n" +
+        Console.WriteLine("Выберите опцию: \n" +
+                          "0 - выйти из программы" +
+                          "1 - из декартовых координат в полярные \n" +
                           "2 - из полярных координат в декартовые \n" +
                           "3 - из декартовых в сферические \n" +
                           "4 - из сферических в декартовы \n" +
                           "5 - из декартовых в цилиндрические \n" +
                           "6 - из цилиндрических в декартовы");
-        int choice = int.Parse(Console.ReadLine());
-        double x, y, z, r, theta, phi;
-        int precision;
-
-        switch (choice)
+        while (true)
         {
-            case 1:
-                Console.Write("Коордианата x = ");
-                x = Convert.ToDouble(Console.ReadLine());
-                Console.Write("Коордианата y = ");
-                y = Convert.ToDouble(Console.ReadLine());
-                Console.Write("Точность = ");
-                precision = Convert.ToInt32(Console.ReadLine());
-                CartesianToPolar(x, y, precision);
-                break;
-
-            case 2:
-                Console.Write("ρ = ");
-                r = Convert.ToDouble(Console.ReadLine());
-                Console.Write("θ = ");
-                phi = Convert.ToDouble(Console.ReadLine());
-                Console.Write("Точность = ");
-                precision = Convert.ToInt32(Console.ReadLine());
-                PolarToCartesian(r, phi, precision);
-                break;
-
-            case 3:
-                Console.Write("Коордианата x = ");
-                x = Convert.ToDouble(Console.ReadLine());
-                Console.Write("Коордианата y = ");
-                y = Convert.ToDouble(Console.ReadLine());
-                Console.Write("Коордианата z = ");
-                z = Convert.ToDouble(Console.ReadLine());
-                Console.Write("Точность = ");
-                precision = Convert.ToInt32(Console.ReadLine());
-                CartesianToSpherical(x, y, z, precision);
-                break;
-            case 4:
-                Console.Write("ρ = ");
-                r = Convert.ToDouble(Console.ReadLine());
-                Console.Write("θ = ");
-                theta = Convert.ToDouble(Console.ReadLine());
-                Console.Write("φ = ");
-                phi = Convert.ToDouble(Console.ReadLine());
-                Console.Write("Точность = ");
-                precision = Convert.ToInt32(Console.ReadLine());
-                SphericalToCartesian(r, theta, phi, precision);
-                break;
+            int choice = int.Parse(Console.ReadLine());
+            double x, y, z, r, theta, phi;
+            int precision;
             
-            case 5:
-                Console.Write("Коордианата x = ");
-                x = Convert.ToDouble(Console.ReadLine());
-                Console.Write("Коордианата y = ");
-                y = Convert.ToDouble(Console.ReadLine());
-                Console.Write("Коордианата z = ");
-                z = Convert.ToDouble(Console.ReadLine());
-                Console.Write("Точность = ");
-                precision = Convert.ToInt32(Console.ReadLine());
-                CartesianToСylindrical(x, y, z, precision);
+            if (choice == 0)
+            {
                 break;
-            case 6:
-                Console.Write("ρ = ");
-                r = Convert.ToDouble(Console.ReadLine());
-                Console.Write("θ = ");
-                theta = Convert.ToDouble(Console.ReadLine());
-                Console.Write("z = ");
-                z = Convert.ToDouble(Console.ReadLine());
-                Console.Write("Точность = ");
-                precision = Convert.ToInt32(Console.ReadLine());
-                СylindricalToCartesian(r, theta, z, precision);
-                break;
+            }
+
+            switch (choice)
+            {
+                case 1:
+                    Console.Write("Коордианата x = ");
+                    x = Convert.ToDouble(Console.ReadLine());
+                    Console.Write("Коордианата y = ");
+                    y = Convert.ToDouble(Console.ReadLine());
+                    Console.Write("Точность = ");
+                    precision = Convert.ToInt32(Console.ReadLine());
+                    CartesianToPolar(x, y, precision);
+                    break;
+
+                case 2:
+                    Console.Write("r = ");
+                    r = Convert.ToDouble(Console.ReadLine());
+                    Console.Write("theta = ");
+                    phi = Convert.ToDouble(Console.ReadLine());
+                    Console.Write("Точность = ");
+                    precision = Convert.ToInt32(Console.ReadLine());
+                    PolarToCartesian(r, phi, precision);
+                    break;
+
+                case 3:
+                    Console.Write("Коордианата x = ");
+                    x = Convert.ToDouble(Console.ReadLine());
+                    Console.Write("Коордианата y = ");
+                    y = Convert.ToDouble(Console.ReadLine());
+                    Console.Write("Коордианата z = ");
+                    z = Convert.ToDouble(Console.ReadLine());
+                    Console.Write("Точность = ");
+                    precision = Convert.ToInt32(Console.ReadLine());
+                    CartesianToSpherical(x, y, z, precision);
+                    break;
+                case 4:
+                    Console.Write("r = ");
+                    r = Convert.ToDouble(Console.ReadLine());
+                    Console.Write("theta = ");
+                    theta = Convert.ToDouble(Console.ReadLine());
+                    Console.Write("phi = ");
+                    phi = Convert.ToDouble(Console.ReadLine());
+                    Console.Write("Точность = ");
+                    precision = Convert.ToInt32(Console.ReadLine());
+                    SphericalToCartesian(r, theta, phi, precision);
+                    break;
+
+                case 5:
+                    Console.Write("Коордианата x = ");
+                    x = Convert.ToDouble(Console.ReadLine());
+                    Console.Write("Коордианата y = ");
+                    y = Convert.ToDouble(Console.ReadLine());
+                    Console.Write("Коордианата z = ");
+                    z = Convert.ToDouble(Console.ReadLine());
+                    Console.Write("Точность = ");
+                    precision = Convert.ToInt32(Console.ReadLine());
+                    CartesianToСylindrical(x, y, z, precision);
+                    break;
+                case 6:
+                    Console.Write("r = ");
+                    r = Convert.ToDouble(Console.ReadLine());
+                    Console.Write("theta = ");
+                    theta = Convert.ToDouble(Console.ReadLine());
+                    Console.Write("z = ");
+                    z = Convert.ToDouble(Console.ReadLine());
+                    Console.Write("Точность = ");
+                    precision = Convert.ToInt32(Console.ReadLine());
+                    СylindricalToCartesian(r, theta, z, precision);
+                    break;
+            }
         }
+        Console.ReadKey();
     }
 }
